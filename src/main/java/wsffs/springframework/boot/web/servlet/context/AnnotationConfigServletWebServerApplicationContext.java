78edea6217e4abc0d16be0a83eaca181c351b8a0
hyeyoom <neoul_chw@icloud.com>
2023-07-17 00:08:15 +0900

    misc: 학습을 위한 주석 정돈

--- src/main/java/wsffs/springframework/boot/web/servlet/context/AnnotationConfigServletWebServerApplicationContext.java
+++ src/main/java/wsffs/springframework/boot/web/servlet/context/AnnotationConfigServletWebServerApplicationContext.java
@@ -46,14 +46,15 @@ public class AnnotationConfigServletWebServerApplicationContext
      * if it fails, to avoid dangling resources. In other words, after invocation
      * of this method, either all or no singletons at all should be instantiated.
      *
+     * 설정의 영속적인 표현을 로드하거나 리프레시 할 수 있습니다. 이는 자바 기반 설정, XML 파일,
+     * 프로퍼티 파일, 관계형 데이터베이스 스키마 스키마 혹은 몇 다른 포맷등에서 가져올 수 있습니다.
+     * 이는 startup 메소드로서, 실패하는 경우 생성된 싱글톤들을 제거하여 댕글링 리소스를 피할 수 있습니다.
+     * 바꿔말하면, 이 메소드가 호출되고 난 후에는 모든 싱글톤 객체가 인스턴스화 되거나 아무것도 인스턴스화 되어선 안됩니다.
+     * (all or nothing)
+     *
      * @throws BeansException        if the bean factory could not be initialized
      * @throws IllegalStateException if already initialized and multiple refresh
      *                               attempts are not supported
-     *                               설정의 영속적인 표현을 로드하거나 리프레시 할 수 있습니다. 이는 자바 기반 설정, XML 파일,
-     *                               프로퍼티 파일, 관계형 데이터베이스 스키마 스키마 혹은 몇 다른 포맷등에서 가져올 수 있습니다.
-     *                               이는 startup 메소드로서, 실패하는 경우 생성된 싱글톤들을 제거하여 댕글링 리소스를 피할 수 있습니다.
-     *                               바꿔말하면, 이 메소드가 호출되고 난 후에는 모든 싱글톤 객체가 인스턴스화 되거나 아무것도 인스턴스화 되어선 안됩니다.
-     *                               (all or nothing)
      */
     public void refresh() throws BeansException {
         scanner.scan(basePackages);
