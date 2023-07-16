98ee45220aba88f50841300ba139863078ccafcb
hyeyoom <neoul_chw@icloud.com>
2023-07-16 19:20:42 +0900

    fixed wrongly tracked files

--- src/main/java/wsffs/springframework/boot/web/servlet/context/AnnotationConfigServletWebServerApplicationContext.java
+++ src/main/java/wsffs/springframework/boot/web/servlet/context/AnnotationConfigServletWebServerApplicationContext.java
@@ -3,7 +3,7 @@ package wsffs.springframework.boot.web.servlet.context;
 import wsffs.springframework.beans.BeansException;
 import wsffs.springframework.context.ApplicationContext;
 
-public class AnnotationConfigServletWebServerApplicationContext  implements ApplicationContext {
+public class AnnotationConfigServletWebServerApplicationContext implements ApplicationContext {
 
     @Override
     public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
